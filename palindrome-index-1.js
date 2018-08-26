55336a77c5ee6975198e41691c0284d5f0f4dcb2
kenpeter <kenpeter@kenpeter>
2018-08-26 01:35:13 +0000

    up

--- palindrome-index-1.js
+++ palindrome-index-1.js
@@ -5,27 +5,40 @@ function getPalindromeIndex(str){
     //crux: compare start and end
     //if start is different with end, check palindrome with removal of both
     //continue till start meets end
+
+	// start index
     var start = 0;
+
+	// end index
     var end = str.length - 1;
+
+	// tmp
     var tmp = '';
-    
+   
+	// we check palindrome straight away 
     if(isPalindrome(str)){
         return -1;
     }
-    
+   
+	// start and end not meet 
     while(start !== end){
+		// start end not same	
         if(str[start] !== str[end]){
-            tmp = replaceAt(str,start,'');
+			// we dry replace, then check
+            tmp = replaceAt(str, start,'');
+			// start, then return
             if(isPalindrome(tmp)){
                 return start;
             }
             
+			// end, then return
             tmp = replaceAt(str,end,'');
             if(isPalindrome(tmp)){
                 return end;
             }         
         }        
-        
+       
+		// good 
         start++;
         end--;
     }
@@ -33,10 +46,15 @@ function getPalindromeIndex(str){
     return -1;    
 }
 
+// floor, run down e.g. 3.5 -> 3
 function isPalindrome(str){
+	// end index
 	var end = str.length - 1;
+	// mid index
 	var mid = Math.floor(str.length / 2);
-	
+
+	// even, all good
+	// old left mid 	
 	for(var i = 0; i < mid; i++){
 		if(str[i] !== str[end - i]){
 			return false;
@@ -45,6 +63,8 @@ function isPalindrome(str){
 	return true;
 }
 
+// give a str, at index, replace with char
+// char most empty
 function replaceAt(str,i,char){
     return str.substring(0,i) + char + str.substring(i+1);
 }
